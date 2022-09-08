b9b1395e2b5020402d87151b0aef2d30c61a0fd0
chandra bachtiar <chandrabahtiar25@gmail.com>
2022-09-08 16:03:57 +0700

    Update CSS file

---
+++ pages/landing.jsx
@@ -0,0 +1,8 @@
+import Landing from "../components/landing"
+export default function About() {
+    return (
+      <h1 className="text-3xl font-bold italic" >
+        <Landing />
+      </h1>
+    )
+  }
